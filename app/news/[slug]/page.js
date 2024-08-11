93d24483df3094cf42b2941728a542f6a7fb32f5
Qamar Abbas <reply.qamar@gmail.com>
2024-08-11 16:01:46 +0500

    basic structure and styling

---
+++ app/news/[slug]/page.js
@@ -0,0 +1,15 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function NewsDetailPage({params}) {
+  const newsItem = DUMMY_NEWS.find((news) => news.slug === params.slug);
+  return (
+  <article className="news-article">
+    <header>
+      <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+      <h1>{newsItem.title}</h1>
+      <time dateTime={newsItem.date}>{newsItem.date}</time>
+    </header>
+    <p>{newsItem.content}</p>
+  </article>
+  );
+}
---
+++ app/news/page.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import { DUMMY_NEWS } from '@/dummy-news';
+
+export default function NewsPage() {
+  return (
+    <div id="news">
+      <h1>News Page</h1>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((news) => (
+          <li key={news.id}>
+            <Link href={`/news/${news.slug}`}>
+              <img src={`/images/news/${news.image}`} alt={news.title} />
+              <span>{news.title}</span>
+              {/* <p>{news.date}</p> */}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
